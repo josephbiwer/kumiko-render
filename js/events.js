ea75c3d83837fbde8ef4cf2c36f207e92b61b62f
Joe Biwer <josephbiwer@gmail.com>
2024-11-14 12:49:41 -0600

    adding reference dot indicator while hovering

---
+++ js/events.js
@@ -0,0 +1,32 @@
+console.log('adding events for canvas');
+
+canvas.addEventListener('mousemove', e => {
+	const mouseCoords = {
+		x: e.layerX / (deltaX / 2),
+		y: e.layerY / (deltaY / 2)
+	}
+
+	const m1 = {
+		x: Math.floor(mouseCoords.x),
+		y: Math.floor(mouseCoords.y)
+	}
+	
+	const m2 = {
+		x: mouseCoords.x > m1.x + 0.5? m1.x + 1: (mouseCoords.x < m1.x - 0.5? m1.x - 1: m1.x),
+		y: mouseCoords.y > m1.y + 0.5? m1.y + 1: (mouseCoords.y < m1.y - 0.5? m1.y - 1: m1.y),
+	}
+
+	console.log(m1);
+	console.log(m2);
+
+	highlightPoint(e.target.getContext("2d"), m1, m2);
+
+});
+
+canvas.addEventListener('mouseleave', e => {
+	highlightPoint(e.target.getContext("2d"), null, null);
+});
+
+canvas.addEventListener('click', e => {
+	console.log("clicked canvas");
+});
--- js/main.js
+++ js/main.js
@@ -11,6 +11,15 @@ const strokeWidth = 2;
 const deltaY = 100;
 const deltaX = 100;
 
+// Rendering logic
+function drawRefDots (ctx, coords, color) {
+	ctx.fillStyle = color;
+	ctx.beginPath();
+	ctx.arc(coords.x * deltaX / 2, coords.y * deltaY / 2, 2, 0, Math.PI * 2, true); // Right eye
+	ctx.closePath();
+	ctx.fill();
+}
+
 function draw4Sides(ctx) {
 	ctx.strokeStyle = "rgb(0 0 0 / 10%)";
 	ctx.lineWidth = strokeWidth;
@@ -25,6 +34,13 @@ function draw4Sides(ctx) {
 	}
 	ctx.closePath();
 	ctx.stroke();
+
+	// Drawing connection points
+	for(let x = 0; x <= 2 * canvasDims.x / deltaX; x++){
+		for(let y = 0; y <= 2 * canvasDims.y / deltaY; y++){
+			drawRefDots(ctx, {x, y}, "rgb(150 0 0 / 100%)");
+		}
+	}
 }
 
 function draw3Sides(ctx) {
@@ -44,18 +60,41 @@ function drawPiece (ctx, start, end) {
 	ctx.strokeStyle = "rgb(0 0 0)";
 	ctx.lineWidth = strokeWidth + 1;
 	ctx.beginPath();
-	ctx.moveTo(start.x, start.y);
-	ctx.lineTo(end.x, end.y);
+	ctx.moveTo(start.x * deltaX / 2, start.y * deltaY / 2);
+	ctx.lineTo(end.x * deltaX / 2, end.y * deltaY / 2);
 	ctx.closePath();
 	ctx.stroke();
 }
 
+let highlight = null;
+function highlightPoint(ctx, c1, c2) {
+	if(c1 !== null) {
+		if(highlight !== null) {
+			// Set previous highlighted one to its default state
+			drawRefDots(ctx, highlight.c1, "rgb(150 0 0 / 100%)");
+			drawRefDots(ctx, highlight.c2, "rgb(150 0 0 / 100%)");
+			// drawRefDots(ctx, highlight, "rgb(0 0 255 / 100%)");
+		}
+
+		// Highlight new selected ref dot
+		highlight = {c1, c2};
+		drawRefDots(ctx, highlight.c1, "rgb(0 255 0 / 100%)");
+		drawRefDots(ctx, highlight.c2, "rgb(0 255 0 / 100%)");
+	} else {
+			drawRefDots(ctx, highlight.c1, "rgb(150 0 0 / 100%)");
+			drawRefDots(ctx, highlight.c2, "rgb(150 0 0 / 100%)");
+			highlight = null;
+	}
+}
+
+// Drawing kumiko panel
 if(canvas.getContext) {
 	const ctx = canvas.getContext("2d");
 
 	// Drawing background of kumiko panel
 	draw3Sides(ctx);
-	// drawPiece(ctx, {x: 0, y:0}, {x: canvasDims.x / deltaX, y: canvasDims.y / deltaY});
+	drawPiece(ctx, {x: 0, y:0}, {x: 1, y: 1});
+	drawPiece(ctx, {x: 1, y:0}, {x: 1, y: 1});
 	
 	// Reference line
 	/*
